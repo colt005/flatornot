c8f474930872c1a9ff15d11b6e66c269e9cfca87
Rohan Prabhu <rohan.prabhu05@gmail.com>
2024-09-30 23:59:54 +0530

    💤 implement graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/colt005/flatornot/server"
 )
@@ -15,6 +19,20 @@ func main() {
 
 	s.RegisterRoutes()
 
-	s.Start()
+	go func() {
+		s.Start()
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	fmt.Println("Shutting down server...")
+
+	//TODO: Flush backlog to DB
+
+	// Graceful shutdown
+	s.ShutDown()
+
+	fmt.Println("Server exiting gracefully")
 
 }
--- server/server.go
+++ server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"context"
+	"log"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/colt005/flatornot/handlers"
 	"github.com/colt005/flatornot/service"
@@ -45,4 +48,14 @@ func (s *Server) Start() {
 	listenAddr := os.Getenv("LISTEN_ADDR")
 	slog.Info("Server Started", "ListenAddress", listenAddr)
 	s.e.Logger.Fatal(s.e.Start(listenAddr))
+}
+
+func (s *Server) ShutDown() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 8*time.Second)
+	defer cancel()
+
+	if err := s.e.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
 }
